eb6c5723ce95e60b7655185713ea42c02537dcba
ChrisG55 <christian_g_55@msn.com>
2013-12-01 18:36:03 +0100

    Construct relationship between articles and ingredients
    This commit adds a many-to-many relationship between articles and
    ingredients. It furthermore adds verifications for article members and
    has some additional test data for development environment as well as a
    default image for articles / ingredients having no images.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ActiveRecord::Base
+  attr_accessible :inci_name :formula :structure :cas_number :ec_number :molecular_weight
+  has_many :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Article.create({:name => "Touch On", :manufacturer => "LOREAL"})
-Article.create({:name => "Root Rescue", :manufacturer => "LOREAL"})
-Article.create({:name => "Healthy Look", :manufacturer => "LOREAL"})
-Article.create({:name => "Instant Color", :manufacturer => "Schwarzkopf"})
-Article.create({:name => "Color & Gloss", :manufacturer => "Schwarzkopf"})
-Article.create({:name => "Intensive Color Creme", :manufacturer => "Schwarzkopf"})
+Article.create({:name => "Prod1", :manufacturer => "Man1", :image => "noimg.svg"})
+Article.create({:name => "Prod2", :manufacturer => "Man1", :image => "noimg.svg"})
+Article.create({:name => "Prod3", :manufacturer => "Man1", :image => "noimg.svg"})
+Article.create({:name => "Prod4", :manufacturer => "Man2", :image => "noimg.svg"})
+Article.create({:name => "Prod5", :manufacturer => "Man2", :image => "noimg.svg"})
+Article.create({:name => "Prod6", :manufacturer => "Man2", :image => "noimg.svg"})
+
+Ingredient.create({:inci_name => "Inci1", :formula => "Form1", :structure => "noimg.svg", :cas_number => "123456", :ec_number => "123456", :molecular_weight => "123.456"})
+Ingredient.create({:inci_name => "Inci2", :formula => "Form2", :structure => "noimg.svg", :cas_number => "134567", :ec_number => "134567", :molecular_weight => "134.567"})
+Ingredient.create({:inci_name => "Inci3", :formula => "Form3", :structure => "noimg.svg", :cas_number => "145678", :ec_number => "145678", :molecular_weight => "145.678"})
+Ingredient.create({:inci_name => "Inci4", :formula => "Form4", :structure => "noimg.svg", :cas_number => "156789", :ec_number => "156789", :molecular_weight => "156.789"})
+Ingredient.create({:inci_name => "Inci5", :formula => "Form5", :structure => "noimg.svg", :cas_number => "167890", :ec_number => "167890", :molecular_weight => "167.890"})
